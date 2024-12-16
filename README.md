b8b8ae3f2c52b63b8aeabfe935f9345ec8b1c12d
Hussein447 <158745152+Hussein447@users.noreply.github.com>
2024-12-16 19:52:05 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+I made this to learn about the Django Web Framework.
+
+This is a simple Kanband Board App.
+
+This features the following:
+
+- Homepage Dashboard
+- Create / Update / Delete Tasks
+- Create / Update / Delete Tasks Comments
+- Task Related Views
+- User Login / Logout
+- User Specific Task Page
+- User can drag and drop Tasks
---
+++ kanban/views/__init__.py
@@ -0,0 +1,8 @@
+from .index import Index
+from .taskDetailView import TaskDetailView
+from .taskForms import TaskCreate, TaskUpdate, TaskDelete
+from .taskCommentForms import TaskCommentCreate, TaskCommentUpdate, TaskCommentDelete
+from .taskActions import TaskAction
+from .kanbanLogin import KanbanLogin
+from .kanbanLogout import KanbanLogout
+from .userTasks import UserTasks
